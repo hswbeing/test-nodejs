28c4bf5808f7d57cae07d5d621f1cd063cf99b9f
Levin Kwong <levinkwong@Levins-MacBook-Pro.local>
2021-03-25 16:42:06 +0800

    Add integration logic

--- saxo-demo/routes/index.js
+++ saxo-demo/routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var fetch = require('node-fetch');
 var router = express.Router();
+var refresh_token = process.env.REFRESH_TOKEN; // "development"
 
 const fs = require('fs');
 
@@ -20,9 +22,72 @@ router.get('/deposits', function(req, res, next) {
   res.send(JSON.stringify(deposits));
 });
 
-router.get('/trusts', function(req, res, next) {
+router.get('/trusts', async function(req, res, next) {
   let trusts = JSON.parse(fs.readFileSync('responses/trusts.json'));
   res.setHeader('Content-Type', 'application/json');
+  console.log("refresh_token: " + refresh_token);
+  console.log(`https://sim.logonvalidation.net/token?grant_type=refresh_token&refresh_token=${refresh_token}&redirect_uri=https://oauth.pstmn.io/v1/callback`)
+  // Call refresh token API
+  const refreshTokenResponse = await fetch(
+    `https://sim.logonvalidation.net/token?grant_type=refresh_token&refresh_token=${refresh_token}&redirect_uri=https://oauth.pstmn.io/v1/callback`, {
+      method: "post",
+      headers: {
+        "Authorization": "Basic OTJiOTczMDExZjI0NDliY2JmMzIxODEyZmYwYmIwMmE6ODFlN2YyYWQ2NzJiNDNhZDlhNTQxMjBhZDZlMGE0ZDI="
+      }
+    }
+  ).catch(err => console.error(err));
+  const refreshTokenResponseJson = await refreshTokenResponse.json();
+  console.log("refreshTokenResponse: " + JSON.stringify(refreshTokenResponseJson));
+  let token = refreshTokenResponseJson.access_token;
+  refresh_token = refreshTokenResponseJson.refresh_token;
+  console.log("token: " + token);
+  console.log("refresh_token: " + refresh_token);
+  // Call Saxo's Net Position API
+  const positionResponse = await fetch(
+    'https://gateway.saxobank.com/sim/openapi/port/v1/netpositions/me/', {
+      method: "get",
+      headers: {
+        "Authorization": `Bearer ${token}`
+      }
+    }
+  ).catch(err => console.error(err));
+  const positionResponseJson = await positionResponse.json();
+  let positions = positionResponseJson.Data;
+  let total = 0;
+  for (const position of positions) {
+    console.log(position)
+    if (position.NetPositionBase.AssetType == 'Bond') {
+      // Call Saxo's Instruments API
+      const instrumentResponse = await fetch(
+        `https://gateway.saxobank.com/sim/openapi/ref/v1/instruments/details/${position.NetPositionBase.Uic}/Bond`, {
+          method: "get",
+          headers: {
+            "Authorization": `Bearer ${token}`
+          }
+        }
+      ).catch(err => console.error(err));
+      const instrument = await instrumentResponse.json();
+      console.log(instrument);
+      let profolio = {
+        "productID": position.NetPositionBase.Uic,
+        "productName": instrument.Description,
+        "riskLevel": "",
+        "prodCcy": instrument.CurrencyCode,
+        "noOfUnits": position.NetPositionBase.Amount,
+        "prevClosePrice": position.NetPositionView.CurrentPrice,
+        "prevClosePriceType": position.NetPositionView.USD,
+        "unitDecimalPoint": 2,
+        "exchangeRate": position.NetPositionView.conversionRateCurrent,
+        "marketValue": position.NetPositionView.MarketValue,
+        "marketValueHKD": position.NetPositionView.MarketValue * 7.8,
+        "marketValueHKDType": "HKD"
+      }
+      total = total + position.NetPositionView.MarketValue;
+      trusts.data.userData.accountDetails[0].portfolioDetails.push(profolio);
+    }
+  }
+  trusts.totalNetAssetValue = total;
+  trusts.totalNetAssetHKDValue = total * 7.8;
   res.send(JSON.stringify(trusts));
 });
 
